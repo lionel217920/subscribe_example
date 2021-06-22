ac0fda0babb0ff91b3e76e167fbe8fa59d20b979
lionel217920 <lionel217920@163.com>
2021-06-22 21:54:05 +0800

    add avro test

---
+++ avro/README.md
@@ -0,0 +1,8 @@
+# avro-tools使用
+
+## 使用arvo生成Java文件
+
+```shell
+java -jar avro-tools-1.8.2.jar compile schema Record.avsc
+```
+
---
+++ avro/compile.sh
@@ -0,0 +1,3 @@
+java -jar avro-tools-1.8.2.jar compile schema test/user.avsc target
+
+# java -jar avro-tools-1.8.2.jar compile schema Record.avsc target
